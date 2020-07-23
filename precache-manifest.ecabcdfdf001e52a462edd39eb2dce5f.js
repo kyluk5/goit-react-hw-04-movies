3bfa6c4236ed3ef7955f9d92a8764503e34eef1c
Andrii Kulyk <kyluk5@gmail.com>
2020-07-23 21:31:15 +0300

    Updates

---
+++ precache-manifest.ecabcdfdf001e52a462edd39eb2dce5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f92ca814be397530970a093c38d26323",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "15f3936d817126e809cc",
+    "url": "/goit-react-hw-04-movies/static/css/main.91db109f.chunk.css"
+  },
+  {
+    "revision": "69ec7d999ad36696ba6d",
+    "url": "/goit-react-hw-04-movies/static/js/2.de4beefd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.de4beefd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f3936d817126e809cc",
+    "url": "/goit-react-hw-04-movies/static/js/main.d714ee2a.chunk.js"
+  },
+  {
+    "revision": "952c08333dad3729b462",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.ad4bcf9c.js"
+  },
+  {
+    "revision": "81a49b036c7a9986558d4aada4f26d97",
+    "url": "/goit-react-hw-04-movies/static/media/Back_Arrow.81a49b03.png"
+  }
+]);
