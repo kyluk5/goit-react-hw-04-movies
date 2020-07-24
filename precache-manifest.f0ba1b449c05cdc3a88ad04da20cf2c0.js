3d66d49d1277c51598856c9cf599207b4f7a8700
Andrii Kulyk <kyluk5@gmail.com>
2020-07-24 17:45:06 +0300

    Updates

---
+++ precache-manifest.f0ba1b449c05cdc3a88ad04da20cf2c0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bd64fcb3c5465fb8fabc0cd180e8fa",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "a745d7cbc4a2a25c8076",
+    "url": "/goit-react-hw-04-movies/static/css/Cast.bf37bfa6.chunk.css"
+  },
+  {
+    "revision": "43ec83bcd38fb7adaafc",
+    "url": "/goit-react-hw-04-movies/static/css/FilmItem.c43d7b7c.chunk.css"
+  },
+  {
+    "revision": "5252663997ced79a48a5",
+    "url": "/goit-react-hw-04-movies/static/css/Movies.e40d936f.chunk.css"
+  },
+  {
+    "revision": "b45f2e218eaa94850e45",
+    "url": "/goit-react-hw-04-movies/static/css/home.c5e59739.chunk.css"
+  },
+  {
+    "revision": "883b0846a49097cc7dae",
+    "url": "/goit-react-hw-04-movies/static/css/main.6c189b76.chunk.css"
+  },
+  {
+    "revision": "9c2800583f6dfa896031",
+    "url": "/goit-react-hw-04-movies/static/js/0.7f99b2ae.chunk.js"
+  },
+  {
+    "revision": "9fd359c5f772fa0fed1c",
+    "url": "/goit-react-hw-04-movies/static/js/8.48d7647a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/8.48d7647a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a745d7cbc4a2a25c8076",
+    "url": "/goit-react-hw-04-movies/static/js/Cast.bf9f1ae7.chunk.js"
+  },
+  {
+    "revision": "43ec83bcd38fb7adaafc",
+    "url": "/goit-react-hw-04-movies/static/js/FilmItem.166d842e.chunk.js"
+  },
+  {
+    "revision": "5252663997ced79a48a5",
+    "url": "/goit-react-hw-04-movies/static/js/Movies.cbe2659b.chunk.js"
+  },
+  {
+    "revision": "565cb1ec2e3b27efe034",
+    "url": "/goit-react-hw-04-movies/static/js/Reviews.8bb7fa7e.chunk.js"
+  },
+  {
+    "revision": "b45f2e218eaa94850e45",
+    "url": "/goit-react-hw-04-movies/static/js/home.ce20ce00.chunk.js"
+  },
+  {
+    "revision": "883b0846a49097cc7dae",
+    "url": "/goit-react-hw-04-movies/static/js/main.944c91fe.chunk.js"
+  },
+  {
+    "revision": "19139cd2901a10109d7f",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.3fc3dd61.js"
+  },
+  {
+    "revision": "81a49b036c7a9986558d4aada4f26d97",
+    "url": "/goit-react-hw-04-movies/static/media/Back_Arrow.81a49b03.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-movies/precache-manifest.370baebe82ba32b1adacea59dff45222.js"
+  "/goit-react-hw-04-movies/precache-manifest.f0ba1b449c05cdc3a88ad04da20cf2c0.js"
 );
 
 self.addEventListener('message', (event) => {
